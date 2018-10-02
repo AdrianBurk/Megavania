7f97cf03fafc60df1f75004ee18ea4f1cf72d159
AdrianBurk <43044023+AdrianBurk@users.noreply.github.com>
2018-10-02 15:19:06 -0500

    MiddleTutorialPT6
    Forgot to commit up until now.
    In the middle of making the jup function.

---
+++ Assets/Scrpits/PlayerCharacter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour 
+{
+    [SerializeField]
+    private float accelerationForce = 5;
+
+    [SerializeField]
+    private float maxSpeed = 5;
+
+    [SerializeField]
+    private float jumpForce = 10;
+
+    [SerializeField]
+    private Rigidbody2D rb2d;
+
+    private float horizontalInput;
+
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        // Gets player input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(Vector2.up * jumpForce)
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        rb2d.AddForce(Vector2.right * horizontalInput * accelerationForce);
+        Vector2 clampedVelocity = rb2d.velocity;
+        clampedVelocity.x = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+        rb2d.velocity = clampedVelocity;
+    }
+}

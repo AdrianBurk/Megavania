f0dd9763bf526a2d8c6a5e6de54659229b5fab63
Adrian Burk <adrian.burk@loop.colum.edu>
2018-12-17 15:38:13 -0600

    Collectables
    Finished Collectables script
    Finished Level2
    Started Level3

--- Assets/Scrpits/Collectable.cs
+++ Assets/Scrpits/Collectable.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class Collectable : MonoBehaviour
 {
     private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+    private static int soulCount = 0;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            soulCount++;
+            Debug.Log("Coin count:" + soulCount);
             audioSource.Play();
-            Destroy(gameObject,audioSource.clip.length);
+            spriteRenderer.enabled = false;
+            boxCollider2D.enabled = false;
+            //Destroy(gameObject,audioSource.clip.length);
         }
     }
 }
